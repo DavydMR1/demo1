de7b7ecb610ca7b1120d683ed4a0e5d8061074d2
davyd <allbuddyes@gmail.com>
2024-10-05 09:11:05 +0300

    a

--- src/main/java/com/example/demo1/services/NotificationService.java
+++ src/main/java/com/example/demo1/services/NotificationService.java
@@ -8,4 +8,7 @@ public class NotificationService {
     public void notifyUser(User user) {
         System.out.println("A new user has been created: " + user.getName());
     }
+    public void notifyOperation(String operation) {
+        System.out.println("Operation " + operation + "has been performed");
+    }
 }
--- src/main/java/com/example/demo1/services/RegistrationService.java
+++ src/main/java/com/example/demo1/services/RegistrationService.java
@@ -14,20 +14,25 @@ public class RegistrationService {
     @Autowired
     private UserService userService;
 
+    @Autowired NotificationService notificationService;
+
     public void registerUser(String name, int age, String email) {
         processingService.recordUser(userService.createUser(name, age, email));
     }
 
     public List<User> sortUsersByAge()
     {
+        notificationService.notifyOperation("sortUsersByAge");
         return processingService.sortUsersByAge(processingService.getUserRepository().getUsers());
     }
 
     public List<User> filterUsersByAge(int age) {
+        notificationService.notifyOperation("filterUsersByAge");
         return processingService.filterUsersByAge(processingService.getUserRepository().getUsers(), age);
     }
 
     public double calculateAverageAge() {
+        notificationService.notifyOperation("calculateAverageAge");
         return processingService.calculateAverageAge(processingService.getUserRepository().getUsers());
     }
 }
